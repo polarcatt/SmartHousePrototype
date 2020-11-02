c1ad35333aae79d15f9556740f136a30ab3d5a2a
polarcatt <pd.ermolaev@gmail.com>
2020-11-02 14:19:23 +0300

    Update Second.java

--- client/SmartHouse/app/src/main/java/com/example/smarthouse/Second.java
+++ client/SmartHouse/app/src/main/java/com/example/smarthouse/Second.java
@@ -10,10 +10,15 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Second extends Activity {
     private  String LOG_TAG   = "SOCKET";
@@ -23,14 +28,37 @@ public class Second extends Activity {
     private  String HOST = "10.120.51.22";
     private  int PORT = 9574;
     private Button close = null;
-    //outputStream:
-    private OutputStream output;
-    //InputStream:
-    private InputStram input;
-    //DataOutputStream
-    private DataOutputStream dos;
-    //DataInputStream
-    private DataInputStream dstream;
+
+    private static OutputStream output;
+    private static InputStream input;
+    private static DataOutputStream dos;
+    private static DataInputStream dstream;
+
+    private Timer timer;
+    private TimerTask timerTask = new TimerTask() {
+
+        @Override
+        public void run() {
+            try {
+                Tick();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    public void startLoop() {
+        if(timer != null) {
+            return;
+        }
+        timer = new Timer();
+        timer.scheduleAtFixedRate(timerTask, 0, 500);
+    }
+
+    public void stopLoop() {
+        timer.cancel();
+        timer = null;
+    }
 
 
     @Override
@@ -40,20 +68,20 @@ public class Second extends Activity {
         Button connect = (Button) findViewById(R.id.button4);
         Button close = (Button) findViewById(R.id.button5);
         close.setEnabled(false);
-
+        startLoop();
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onOpenClick();
             }
         });
-
         close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onCloseClick();
             }
         });
+
     }
     public void onOpenClick()
     {
@@ -113,15 +141,10 @@ public class Second extends Activity {
             // Если сокет уже открыт, то он закрывается
             closeConnection();
             try {
-                // Создание сокета
                 mSocket = new Socket(mHost, mPort);
-                // output
                 output = mSocket.getOutputStream();
-                // input
-                input = mSocket.getInputSteam();
-                // dos
+                input = mSocket.getInputStream();
                 dos = new DataOutputStream(output);
-                // dstream
                 dstream = new DataInputStream(input);
             } catch (IOException e) {
                 throw new Exception("Невозможно создать сокет: "
@@ -159,50 +182,58 @@ public class Second extends Activity {
     public void onSwitchClick(View v)
     {
         Switch swi = (Switch) findViewById(R.id.switch1);
+        swi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+            {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                try {
+                    if (isChecked) {
+                        dos.writeBytes("turnOn");
+                    } else {
+                        dos.writeBytes("turnOff");
+                    }
+                }catch (IOException e){
 
+                }
+            }
+        }
+        );
+    }
+
+    public String GetReceivedData() throws IOException {
+        // читает строку с сервера
+        if(dstream != null && dstream.available() > 0) {
+            String line;
+            line = dstream.readUTF();
+            // проверка состояния лампочки
+            return line;
+        }else return "";
+    }
+
+    public void Tick() throws IOException {
+        String data = GetReceivedData();
+        ApplyState(data.equals("On"));
+    }
+
+    public void ApplyState(boolean state)
+    {
+        Switch swi = (Switch) findViewById(R.id.switch1);
+        swi.setOnCheckedChangeListener(null);
+        swi.setChecked(state);
         swi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
         {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
             {
+                try {
+                    if (isChecked) {
+                        dos.writeBytes("turnOn");
+                    } else {
+                        dos.writeBytes("turnOff");
+                    }
+                }catch (IOException e){
 
-                if (isChecked) {
-                    dos.writeBytes("turnOn");
-                }
-                else {
-                    dos.writeBytes("turnOff");
                 }
             }
-        }
-    }
-
-    public GetReceivedData()
-    {
-        // читает строку с сервера
-        String line;
-        line = dstream.readline();
-        // проверка состояния лампочки
-        if (line == "On") {
-            ApplyState(true);
-        } else if (line == "Off") {
-            ApplyState(false);
-        }
-    }
-
-    public Tick()
-    {
-        new Thread(new Runnable() {
-        @Override
-            public void run() {
-                // вызов функции состояния
-                GetReceivedData();
-            }
-        }).start();
-    }
-
-    public boolean ApplyState(boolean state)
-    {
-        swi.setOnCheckedChangeListener(null);
-        swi.setOnChecked(state);
-        swi.setOnCheckedChangeListener(this);
+        });
     }
 }
